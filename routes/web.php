f1113ecc1a606d07074eede9f9e1570194b138b5
samba <mavie1977@yahoo.fr>
2026-06-28 20:45:45 +0200

    Restore authentication routes for V7

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,18 @@ use App\Http\Controllers\Portal\ThemeController;
 use App\Http\Controllers\Portal\DemarcheController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\Manager\ManagerDashboardController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [PortalController::class, 'index'])->name('portal.home');
 Route::get('/themes', [ThemeController::class, 'index'])->name('themes.index');
 Route::get('/themes/{theme}', [ThemeController::class, 'show'])->name('themes.show');
 Route::get('/demarches/{demarche}', [DemarcheController::class, 'show'])->name('demarches.show');
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); 
 
 Route::middleware(['snae.auth'])->group(function () {
     Route::post('/demarches/{demarche}/demande', [DemarcheController::class, 'store'])->name('demarches.store');
